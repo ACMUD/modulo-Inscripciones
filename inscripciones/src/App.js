4e7f23d9313ab37adb81ffa110401545715c1749
EdisonP Peñuela <edisonpe961206@hotmail.com>
2019-05-04 14:44:50 -0500

    added basic header

--- inscripciones/src/App.js
+++ inscripciones/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './header/header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ inscripciones/src/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+function Header(props) {
+    return (
+        <header className='App-header'>
+            <h1>Inscripciones a los grupos de Trabajo</h1>
+        </header >
+    )
+}
+
+export default Header;
