8c2acb81119af66ff207ad40ea82528903c2b1be
EdisonP Peñuela <edisonpe961206@hotmail.com>
2019-05-11 16:31:39 -0500

    Added route to SingUp component

--- inscripciones/src/contentApp/contentApp.js
+++ inscripciones/src/contentApp/contentApp.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import SingUp from './Sing-Up-In/containers/singUp'
 
 class ContentApp extends React.Component {
     constructor(props) {
@@ -13,7 +14,7 @@ class ContentApp extends React.Component {
         return (
             <BrowserRouter style={{ width: '100vw', height: '100%' }}>
                 <Switch>
-                    <Route exact path='/' Component={null} />
+                    <Route exact path='/SingUp' component={SingUp} />
                 </Switch>
             </BrowserRouter>
         )
