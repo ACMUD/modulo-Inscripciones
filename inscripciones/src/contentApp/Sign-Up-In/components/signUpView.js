1b2750bf16dc0a3d1ac6cc4089554fa53097c2ea
EdisonP Peñuela <edisonpe961206@hotmail.com>
2019-05-15 02:15:32 -0500

    finished the register module

--- inscripciones/src/contentApp/Sign-Up-In/components/signUpView.js
+++ inscripciones/src/contentApp/Sign-Up-In/components/signUpView.js
@@ -1,22 +1,35 @@
 import React from 'react'
 import '../../contentApp.css';
-import TextField from '@material-ui/core/TextField';
+import { TextField } from '@material-ui/core'
+import { NativeSelect, InputLabel, OutlinedInput, FormControl } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 
 function SignUpView(props) {
     return (
-        <div>
-            {textInputField("Nombres", props.data.Nombres, props.handleChange, props.error)}
-            {textInputField("Apelidos", props.data.Apellidos, props.handleChange, props.error)}
-            {textInputField("Codigo", props.data.Codigo, props.handleChange, props.error)}
-            {textInputField("Telefono", props.data.Telefono, props.handleChange)}
+        <div style={{ height: '100%' }}>
+            <div style={{ height: '85%', display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly', alignContent: 'center' }}>
+                {textInputField("Nombres", "Nombres", props.data.Nombres, props.handleChange, props.error)}
+                {textInputField("Apellidos", "Apellidos", props.data.Apellidos, props.handleChange, props.error)}
+                {selectInputField("Tipo de ID", "TipoIdentificacion", props.data.TipoIdentificacion, props.handleChange, props.data.TiposIdentificacion, props.error)}
+                {textInputField("Identificación", "NumeroIdentificacion", props.data.NumeroIdentificacion, props.handleChange, props.error)}
+                {textInputField("Código", "Codigo", props.data.Codigo, props.handleChange, props.error)}
+                {textInputField("Teléfono", "Telefono", props.data.Telefono, props.handleChange)}
+                {selectInputField("Tipo de Correo", "TipoCorreo", props.data.TipoCorreo, props.handleChange, props.data.TiposCorreo, props.error)}
+                {textInputField("Correo", "Correo", props.data.Correo, props.handleChange, props.error)}
+            </div>
+            <div>
+                <Button variant="contained" color="primary" onClick={props.sendData}>
+                    Registrarse
+                </Button>
+            </div>
         </div>
     )
 }
 
-function textInputField(variableName, variableValue, handleChange, error = false) {
+function textInputField(label, variableName, variableValue, handleChange, error = false) {
     return (
         <TextField
-            label={variableName}
+            label={label}
             name={variableName}
             value={variableValue}
             onChange={(event) => handleChange(event.target.name, event.target.value)}
@@ -24,6 +37,40 @@ function textInputField(variableName, variableValue, handleChange, error = false
             variant="outlined"
             error={(error && variableValue === '')}
         />
+    )
+}
+
+function selectInputField(label, variableName, variableValue, handleChange, options, error = false) {
+    return (
+        <FormControl
+            variant="outlined"
+            style={{ marginTop: '16px', marginBottom: '8px', minWidth: '242px' }}
+            error={(error && variableValue === '')}
+        >
+            <InputLabel>
+                {label}
+            </InputLabel>
+            <NativeSelect
+                name={variableName}
+                value={variableValue || ''}
+                onChange={(event) => handleChange(event.target.name, event.target.value)}
+                input={
+                    <OutlinedInput
+                        labelWidth={variableName.length * 4}
+                        name={variableName}
+                    />
+                }
+            >
+                <option value={''} />
+                {
+                    options.map((opt, index) => {
+                        return (
+                            <option key={index} value={opt.Id}>{opt.Abreviacion || opt.TipoCorreo}</option>
+                        )
+                    })
+                }
+            </NativeSelect>
+        </FormControl>
     )
 }
 
--- inscripciones/src/contentApp/Sign-Up-In/containers/signUp.js
+++ inscripciones/src/contentApp/Sign-Up-In/containers/signUp.js
@@ -1,19 +1,48 @@
 import React from 'react'
 import SignUpView from '../components/signUpView'
+import { requesterCrudServer } from '../../../Resources/requester'
 
 class SignUp extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            // Por Defecto Activo
+            EstadoUsuario: null,
+
+            // Datos Tabla Persona
             Nombres: "",
             Apellidos: "",
             Codigo: "",
             Telefono: "",
-        }
+            // Response (result.body.Id)
+            PersonaID: 0,
 
-        this.EstadoUsuario = {
-            "Id": 1
+            // Datos Tabla identificacion (Requiere Persona ID y tipoIdentificación ID)
+            NumeroIdentificacion: "",
+            TipoIdentificacion: "",
+
+            // Datos Tabla Correo (Lista:) (requiere Persona ID y tipoCorreo ID (evaluar casos))
+            Correo: "", // 'Correo' c/u
+            TipoCorreo: "",
+
+            // rol (por defecto) (rol_persona requiere id rol id persona)
+            Rol: '',
+
+            // Opciones
+            TiposIdentificacion: [],
+            TiposCorreo: [],
+
+            error: false,
+            networkError: false,
         }
+    }
+
+    componentWillMount() {
+        this.getEstadosUsuario()
+
+        this.getTipoIdentificacion()
+        this.getTipoCorreo()
+        this.getRol()
     }
 
     render() {
@@ -21,9 +50,182 @@ class SignUp extends React.Component {
             <SignUpView
                 data={this.state}
                 handleChange={(name, value) => this.setState({ [name]: value })}
-                error={false}
+                sendData={this.sendData.bind(this)}
+                error={this.state.error}
             />
         )
+    }
+
+    getEstadosUsuario() {
+        requesterCrudServer('get', 'estado_usuario/?query=Nombre:Activo',
+            (result) => {
+                this.setState({ EstadoUsuario: result.data[0] })
+            },
+            (error) => {
+                console.error(error)
+                this.setState({ networkError: true })
+            }
+        )
+    }
+
+    getTipoIdentificacion() {
+        requesterCrudServer('get', 'tipo_identificacion/',
+            (result) => {
+                this.setState({ TiposIdentificacion: result.data })
+            },
+            (error) => {
+                console.error(error)
+                this.setState({ networkError: true })
+            }
+        )
+    }
+
+    getTipoCorreo() {
+        requesterCrudServer('get', 'tipo_correo/',
+            (result) => {
+                this.setState({ TiposCorreo: result.data })
+            },
+            (error) => {
+                console.error(error)
+                this.setState({ networkError: true })
+            }
+        )
+    }
+
+    getRol() {
+        requesterCrudServer('get', 'rol/?query=Nombre:Estudiante',
+            (result) => {
+                this.setState({ Rol: result.data[0] })
+            },
+            (error) => {
+                console.error(error)
+                this.setState({ networkError: true })
+            }
+        )
+    }
+
+    sendData() {
+        if (this.validateInputs()) {
+            let {
+                EstadoUsuario,
+                Nombres,
+                Apellidos,
+                Codigo,
+                Telefono
+            } = this.state
+
+            requesterCrudServer('post', 'persona/',
+                (result) => {
+                    console.log(result)
+
+                    let personaID = result.data.Body.Id
+
+                    this.sendIdentificacion(personaID)
+                    this.sendCorreo(personaID)
+                    this.sendRol(personaID)
+                },
+                (error) => {
+                    console.error(error)
+                    this.setState({ networkError: true })
+                },
+                {
+                    Nombres,
+                    Apellidos,
+                    Codigo,
+                    Telefono,
+                    EstadoUsuario,
+                }
+            )
+        }
+    }
+
+    sendIdentificacion(PersonaID) {
+        let {
+            NumeroIdentificacion,
+            TipoIdentificacion
+        } = this.state
+
+        requesterCrudServer('post', 'identificacion/',
+            (result) => {
+                console.log(result)
+            },
+            (error) => {
+                console.error(error)
+                this.setState({ networkError: true })
+            },
+            {
+                NumeroIdentificacion,
+                Persona: {
+                    Id: PersonaID
+                },
+                TipoIdentificacion: {
+                    Id: parseInt(TipoIdentificacion),
+                }
+            }
+        )
+    }
+
+    sendCorreo(PersonaID) {
+        let {
+            Correo,
+            TipoCorreo
+        } = this.state
+
+        requesterCrudServer('post', 'correo/',
+            (result) => {
+                console.log(result)
+            },
+            (error) => {
+                console.error(error)
+                this.setState({ networkError: true })
+            },
+            {
+                Correo,
+                Persona: {
+                    Id: PersonaID,
+                },
+                TipoCorreo: {
+                    Id: parseInt(TipoCorreo),
+                }
+            }
+        )
+    }
+
+    sendRol(personaID) {
+        let {
+            Rol
+        } = this.state
+
+        requesterCrudServer('post', 'rol_persona/',
+            (result) => {
+                console.log(result)
+            },
+            (error) => {
+                console.error(error)
+                this.setState({ networkError: true })
+            },
+            {
+                Persona: {
+                    Id: personaID,
+                },
+                Rol
+            }
+        )
+    }
+
+    validateInputs() {
+        if ((this.state.Nombres === "")
+            || (this.state.Apellidos === "")
+            || (this.state.Codigo === "")
+            || (this.state.NumeroIdentificacion === "")
+            || (this.state.TipoIdentificacion === "")
+            || (this.state.Correo === "")
+            || (this.state.TipoCorreo === "")
+        ) {
+            this.setState({ error: true })
+            return false
+        }
+        return true
     }
 }
 
